600150913a48ac974c51a7d4a652b8ea530c9978
azimari.toure.ikbal <azimari.toure.etu@esmt.sn>
2025-03-25 14:43:34 +0000

    added tech stack

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Contact from "@/components/contact";
 import Hero from "@/components/hero";
 import Projects from "@/components/projects";
+import Technologies from "@/components/technologies";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white">
       <Hero />
+      <Technologies />
       <Projects />
       <Contact />
     </main>
---
+++ components/technologies.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import React from "react";
+import { Badge } from "./ui/badge";
+
+type TechnologiesProps = {};
+
+// Tech stacks data
+const firstRowTech = [
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Hono",
+  "Vercel",
+  "Tailwind CSS",
+  "Java",
+  "PostgreSQL",
+  "Trpc",
+  "Docker",
+  "Git",
+  "React-query",
+  "React-tables",
+  "Zod",
+  "React-hook-form",
+];
+
+const Technologies: React.FC<TechnologiesProps> = ({}) => {
+  return (
+    <section id="projects" className="py-20 bg-black">
+      <div className="container mx-auto px-4">
+        <div className="mb-16 text-center">
+          <h2 className="text-3xl md:text-5xl font-bold mb-6">Technologies</h2>
+          <p className="text-silver max-w-2xl mx-auto mb-10">
+            Une sélection de mes travaux récents sur diverses technologies.
+          </p>
+
+          <div className="space-y-2">
+            <div className="w-full space-x-2 inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+              <ul className="flex items-center gap-2 justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+                {firstRowTech.map((tech, index) => (
+                  <Badge key={index}>{tech}</Badge>
+                ))}
+              </ul>
+              <ul
+                className="flex items-center gap-2 justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+                aria-hidden={true}
+              >
+                {firstRowTech.map((tech, index) => (
+                  <Badge key={index}>{tech}</Badge>
+                ))}
+              </ul>
+            </div>
+
+            <div className="w-full space-x-2 inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+              <ul className="flex items-center gap-2 justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none delay-1000 animate-infinite-scroll">
+                {firstRowTech.map((tech, index) => (
+                  <Badge key={index}>{tech}</Badge>
+                ))}
+              </ul>
+              <ul
+                className="flex items-center gap-2 justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none delay-1000 animate-infinite-scroll"
+                aria-hidden={true}
+              >
+                {firstRowTech.map((tech, index) => (
+                  <Badge key={index}>{tech}</Badge>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Technologies;
